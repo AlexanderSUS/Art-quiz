15835797ba7ea78d400fcce063744b70b27f7d47
Alexander Suslov <sasha_suslov07@mail.ru>
2022-09-01 18:14:30 +0300

    refactor: remove comments

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import View from './utils/view';
 import Model from './utils/model';
 import Controller from './utils/controller';
 import Quiz from './utils/quiz';
-// import images from './utils/images';
-// import dictionary from './utils/dictionary';
-// import categories from './utils/categories';
 import components from './utils/components';
 import pages from './utils/pages';
 
