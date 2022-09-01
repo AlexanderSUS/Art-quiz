96eed2aa50349c45c4a1e24a6ec7fae91bebbcdd
Alexander Suslov <sasha_suslov07@mail.ru>
2022-09-01 22:03:50 +0300

    refactor: footer styles

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -2,16 +2,10 @@ import './index.scss';
 
 const footer = document.createElement('footer');
 footer.classList.add('home');
-footer.innerHTML = `<div class="app-info">
-                            <div class="rs-logo">
-                              <a href="https://rs.school/js/"><img src="./assets/rs_school_js.svg" alt="rs school courses"></a>
-                            </div>
-                            <div class="app-details">
-                              <a href="https://github.com/AlexanderSUS">
-                                <span class="app-author">Alexander Suslov</span>
-                                <span class="app-year"> 2021</span>
-                              </a>
-                            </div>
-                          </div>`;
+footer.innerHTML = `<a class="rs-logo" href="https://rs.school/js/"><img src="./assets/rs_school_js.svg" alt="rs school courses"></a>
+                    <a class="app-details" href="https://github.com/AlexanderSUS">
+                      <span class="app-author">Alexander Suslov</span>
+                      <span class="app-year"> 2021</span>
+                    </a>`;
 
 export default footer;
